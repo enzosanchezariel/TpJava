51dc51503d2e467aceadc780cba60b88a5c14cf3
Enzo Ariel <enzo.sanchez.ariel@gmail.com>
2024-09-16 22:37:48 -0300

    Login con JDBC

--- src/data/UserDB.java
+++ src/data/UserDB.java
@@ -1,5 +1,9 @@
 package data;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import entities.User;
@@ -7,26 +11,137 @@ import entities.User;
 public class UserDB {
 	
 	public ArrayList<User> getAll(){
-		return null;
+		String sqlSelect = "select * from users";
+		Connect connect = new Connect();
+		Connection con = connect.getConnection();
+		ArrayList<User> users = new ArrayList<User>();
+		
+		try {
+			PreparedStatement stm = con.prepareStatement(sqlSelect);
+			ResultSet rs = stm.executeQuery();
+			while (rs.next()) {
+				int id = rs.getInt("id");
+				String name = rs.getString("name");
+				String surname = rs.getString("surname");
+				String email = rs.getString("email");
+				String password = rs.getString("password");
+				boolean deleted = rs.getBoolean("deleted");
+				users.add(new User(id, name, surname, email, password, deleted));
+			}
+			con.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return users;
 	}
 	
 	public User getById(User u) {
-		return u;
+		String sqlSelect = "select * from users where id = ?";
+		Connect connect = new Connect();
+		Connection con = connect.getConnection();
+		User user = null;
+		
+		try {
+			PreparedStatement stm = con.prepareStatement(sqlSelect);
+            stm.setInt(1, u.getId());
+			ResultSet rs = stm.executeQuery();
+			if (rs != null) {
+				rs.next();
+				int id = rs.getInt("id");
+				String name = rs.getString("name");
+				String surname = rs.getString("surname");
+				String email = rs.getString("email");
+				String password = rs.getString("password");
+				boolean deleted = rs.getBoolean("deleted");
+				user = new User(id, name, surname, email, password, deleted);
+			}
+			con.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return user;
 	}
 	
 	public User getByEmail(User u) {
-		return u;
+		String sqlSelect = "select * from users where email = ?";
+		Connect connect = new Connect();
+		Connection con = connect.getConnection();
+		User user = null;
+		
+		try {
+			PreparedStatement stm = con.prepareStatement(sqlSelect);
+            stm.setString(1, u.getEmail());
+			ResultSet rs = stm.executeQuery();
+			if (rs != null) {
+				rs.next();
+				int id = rs.getInt("id");
+				String name = rs.getString("name");
+				String surname = rs.getString("surname");
+				String email = rs.getString("email");
+				String password = rs.getString("password");
+				boolean deleted = rs.getBoolean("deleted");
+				//System.out.println(deleted);
+				user = new User(id, name, surname, email, password, deleted);
+			}
+			con.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return user;
 	}
 	
 	public void save(User u) {
-		// TODO
+		String sqlSelect = "insert into users(name, surname, email, password) values(?, ?, ?, ?)";
+		Connect connect = new Connect();
+		Connection con = connect.getConnection();
+		try {
+			PreparedStatement stm = con.prepareStatement(sqlSelect);
+			stm.setString(1, u.getName());
+			stm.setString(2, u.getSurname());
+			stm.setString(3, u.getEmail());
+			stm.setString(4, u.getPassword());
+			stm.executeUpdate();
+			con.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public void delete(User u) {
-		// TODO
+		String sqlSelect = "update users set deleted = 1 where id = ?";
+		Connect connect = new Connect();
+		Connection con = connect.getConnection();
+		try {
+			PreparedStatement stm = con.prepareStatement(sqlSelect);
+			stm.setInt(1, u.getId());
+			stm.executeUpdate();
+			con.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public void update(User u) {
-		// TODO
+		String sqlSelect = "update users set name = ?, surname = ?, email = ?, password = ? where id = ?";
+		Connect connect = new Connect();
+		Connection con = connect.getConnection();
+		try {
+			PreparedStatement stm = con.prepareStatement(sqlSelect);
+			stm.setString(1, u.getName());
+			stm.setString(2, u.getSurname());
+			stm.setString(3, u.getEmail());
+			stm.setString(4, u.getPassword());
+			stm.setInt(5, u.getId());
+			stm.executeUpdate();
+			con.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
--- src/logic/UserLogic.java
+++ src/logic/UserLogic.java
@@ -2,32 +2,45 @@ package logic;
 
 import java.util.ArrayList;
 
+import data.UserDB;
 import entities.User;
 
 
 public class UserLogic {
+	
+	private UserDB db = new UserDB();
+	
 	public ArrayList<User> getAll(){
-		// TODO
-		ArrayList<User> a = new ArrayList<User>();
-		
-		a.add(new User(1, "Ricardo", "Darín", "darin.carp@gmail.com", "asdasd", false));
-		a.add(new User(2, "Mark", "Zuckerberg", "area51.boy@meta.com", "dadada", false));
-		a.add(new User(3, "Gustavo", "Cerati", "gustavo.stereo@yahoo.com", "americanBlind", false));
-		a.add(new User(4, "Diego", "Maradona", "d10.maradona@hotmail.com", "footballHigh", false));
-		
-		return a;
+		return db.getAll();
 	}
 	
 	public User getById(User u) {
-		// TODO
-		return new User();
+		return db.getById(u);
+	}
+	
+	public User getByEmail(User u) {
+		return db.getByEmail(u);
+	}
+	
+	public void save(User u) {
+		db.save(u);
+	}
+	
+	public void delete(User u) {
+		db.delete(u);
+	}
+	
+	public void update(User u) {
+		db.update(u);
 	}
 	
 	public User searchAndCompare(User u) {
-		// TODO
-		User testUser = new User(5, "Mario", "Bros", "itsame.mario@yahoo.com", "solong", false);
-		if (u.getEmail().equals(testUser.getEmail()) && u.getPassword().equals(testUser.getPassword())) {
-			return testUser;
+		User userFromDB = db.getByEmail(u);
+		if (userFromDB != null
+				&& !userFromDB.isDeleted()
+				&& u.getEmail().equals(userFromDB.getEmail())
+				&& u.getPassword().equals(userFromDB.getPassword())) {
+			return userFromDB;
 		}
 		else {
 			return null;
