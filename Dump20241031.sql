ba46c2916f9720d26bfc53e5192ef77008a551b5
LautiDuarte <lautaroduarte@outlook.es>
2024-11-04 02:34:58 -0300

    Actualizacion de BD

--- Dump20241031.sql
+++ Dump20241031.sql
@@ -192,7 +192,7 @@ DROP TABLE IF EXISTS `rooms`;
 CREATE TABLE `rooms` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `code` int NOT NULL,
+  `code` varchar(36) NOT NULL,
   `max_amount_participants` int NOT NULL,
   `init_date` date NOT NULL,
   `end_date` date NOT NULL,
